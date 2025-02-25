29cf8f3e12d3898a190429c770e84341c84a2e70
Premkumar R <premkumar.r@ctlep.local>
2025-02-25 11:34:15 +0530

    files added

---
+++ src/main/java/com/example/demo/SpringNew1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNew1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNew1Application.class, args);
+	}
+
+}
